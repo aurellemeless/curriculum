6b9d97d718cc548597fdc88aad47190fd8dc6605
Aurelle Meless <aurelle.meless@gmail.com>
2024-04-29 08:37:18 +0200

    use http request to load data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 import Curriculum from '~/app/components/ui/curriculum';
-import CvData from '~/app/data/cvdata.json';
 import { useEffect, useState } from 'react';
 import { EditOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Spin } from 'antd';
 import { LOCAL_STORAGE } from './constants/constants';
 import { App } from './components/ui/app/App';
+import axios from 'axios';
+import { CVData } from './components/ui/curriculum/types';
 
 export default function Home() {
 	const [isAppOpen, setIsAppOpen] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [CvData, setCvData] = useState({} as CVData);
 	const handleCurriculumClick = (ev: any) => {
 		ev.preventDefault();
 		if (isAppOpen) {
@@ -24,7 +27,13 @@ export default function Home() {
 	 * for dev purposes : should be removed later
 	 */
 	const loadDummyOnLocal = () => {
-		localStorage.setItem(LOCAL_STORAGE, JSON.stringify(CvData));
+		setLoading(true);
+		axios.get('/data/cv.json').then(function ({ data }) {
+			console.log(data);
+			localStorage.setItem(LOCAL_STORAGE, JSON.stringify(data));
+			setCvData(data);
+			setLoading(false);
+		});
 	};
 	useEffect(() => {
 		loadDummyOnLocal();
@@ -32,24 +41,31 @@ export default function Home() {
 
 	return (
 		<main>
-			<div className='page-wrapper'>
-				<div className='curriculum' onClick={handleCurriculumClick}>
-					{!isAppOpen && (
-						<div style={{ display: 'flex', justifyContent: 'center', margin: 10 }}>
-							<Button htmlType='button' onClick={handleEditClick}>
-								Edit resume
-								<EditOutlined />
-							</Button>
+			{loading && (
+				<Spin tip='Loading' size='large'>
+					<div className='content' />
+				</Spin>
+			)}
+			{!loading && (
+				<div className='page-wrapper'>
+					<div className='curriculum' onClick={handleCurriculumClick}>
+						{!isAppOpen && (
+							<div style={{ display: 'flex', justifyContent: 'center', margin: 10 }}>
+								<Button htmlType='button' onClick={handleEditClick}>
+									Edit resume
+									<EditOutlined />
+								</Button>
+							</div>
+						)}
+						<Curriculum {...CvData} />
+					</div>
+					{isAppOpen && (
+						<div className='app'>
+							<App />
 						</div>
 					)}
-					<Curriculum {...CvData} />
 				</div>
-				{isAppOpen && (
-					<div className='app'>
-						<App />
-					</div>
-				)}
-			</div>
+			)}
 		</main>
 	);
 }
